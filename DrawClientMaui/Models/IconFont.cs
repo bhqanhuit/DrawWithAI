10ebb564e29a7b769879d3ae6163fc91cfa9b753
Diem1070 <diemhuynh1070@gmail.com>
2024-11-09 17:21:11 +0700

    Edit UI styles

---
+++ DrawClientMaui/Models/IconFont.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawClientMaui.Models
+{
+    public class IconFont
+    {
+        public static string Visibility = "\ue8f4";
+        public static string Visibility_off = "\ue8f5";
+
+        public static string Photo_library = "\ue413";
+
+        public static string Home = "\ue88a";
+        public static string Home_filled = "\ue9b2";
+        public static string Home_max = "\uf024"; 
+        public static string Home_mini = "\uf025";
+
+        public static string Settings = "\ue8b8";
+        public static string Settings_applications = "\ue8b9";
+        public static string Settings_backup_restore = "\ue8ba";
+        public static string Settings_bluetooth = "\ue8bb";
+        public static string Settings_cell = "\ue8bc";
+        public static string Settings_display = "\ue8bd";
+        public static string Settings_ethernet = "\ue8be";
+        public static string Settings_input_antenna = "\ue8bf";
+        public static string Settings_input_component = "\ue8c0";
+        public static string Settings_input_composite = "\ue8c1";
+        public static string Settings_input_hdmi = "\ue8c2";
+        public static string Settings_input_svideo = "\ue8c3";
+        public static string Settings_overscan = "\ue8c4";
+        public static string Settings_phone = "\ue8c5";
+        public static string Settings_power = "\ue8c6";
+        public static string Settings_remote = "\ue8c7";
+        public static string Settings_voice = "\ue8c8";
+
+        public static string Draw = "\ue746";
+        public static string Edit = "\ue3c9";
+        public static string Edit_square = "\uf88d";
+
+
+    }
+}
